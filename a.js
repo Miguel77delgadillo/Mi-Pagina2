2b136a6804bc8ccc07d6ba799f130a22ace3a2ee
Miguel77delgadillo <fabrizio.delgadillomiranda@intjem.edu.bo>
2026-02-23 09:55:31 -0400

    Initial commit

---
+++ a.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Sitio cargado correctamente");
+
+    setTimeout(() => {
+        alert("Bienvenido a mi Portafolio Académico 2026");
+    }, 2000);
+});
